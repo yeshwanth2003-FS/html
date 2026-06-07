a67760a4fd8a032c75d6ebef48a31cea9cd011df
yeshwanth2003-FS <yeshwanthyeshwanth2003@gmail.com>
2026-06-08 01:22:41 +0530

    updated

---
+++ JAVASCRIPT/assests/js/intro.js
@@ -0,0 +1,320 @@
+
+var visualstudio;
+console.log(visualstudio);
+
+var companywebsite;
+console.log(companywebsite);
+
+var navbar;
+console.log(navbar);
+
+var yourname;
+console.log( yourname);
+
+var fathersname;
+console.log(fathersname);
+
+var mothersname;
+console.log(mothersname);
+
+var yourage;
+console.log(yourage);
+
+var youremail;
+console.log(youremail);
+
+var mylaptop;
+console.log(mylaptop);
+
+var mobilephone;
+console.log(mobilephone);
+
+var chennaiairpot;
+console.log(chennaiairpot);
+
+var socialmedia;
+console.log(socialmedia);
+
+var mobileapp;
+console.log(mobileapp);
+
+var youtube;
+console.log(youtube);
+
+var boyshostel;
+console.log(boyshostel);
+
+var mobiledisplay;
+console.log(mobiledisplay);
+
+var laptopdisplay;
+console.log(laptopdisplay);
+
+var laptopkeyboard;
+console.log(laptopkeyboard);
+
+var sofaset;
+console.log(sofaset);
+
+var bedroom;
+console.log(bedroom);
+
+let football;
+console.log(football);
+
+let notebook;
+console.log(notebook);
+
+let javascript;
+console.log(javascript);
+
+let metrotarin;
+console.log(metrotarin);
+
+let houseowner;
+console.log(houseowner);
+
+let historybook;
+console.log(historybook);
+
+let maindoor;
+console.log(maindoor);
+
+let waterbottel;
+console.log(waterbottel);
+
+let failedmoment;
+console.log(failedmoment);
+
+let skillupgrade;
+console.log(skillupgrade);
+
+let englishcommunication;
+console.log(englishcommunication);
+
+let reactjs;
+console.log(reactjs);
+
+let pythonlanguage;
+console.log(pythonlanguage);
+
+let javalanguage;
+console.log(javalanguage);
+
+let database;
+console.log(database);
+
+let datatransfer;
+console.log(datatransfer);
+
+let filetransfer;
+console.log(filetransfer);
+
+let filelength;
+console.log(filelength);
+
+let keypoints;
+console.log(keypoints);
+
+let toppackage;
+console.log(toppackage);
+
+let annaualpackage;
+console.log(annaualpackage);
+
+let htmltag;
+console.log(htmltag);
+ 
+let airbuds;
+console.log(airbuds);
+
+let intracity;
+console.log(intracity);
+
+let zingbus;
+console.log(zingbus);
+
+let softdrinks;
+console.log(softdrinks);
+
+let fastfood;
+console.log(fastfood);
+
+let bikeparts;
+console.log(bikeparts);
+
+let listnumber;
+console.log(listnumber);
+
+let tupleset;
+console.log(tupleset);
+
+const typeover;
+console.log(typeover);
+
+const chatgpt;
+console.log(chatgpt);
+
+const markup;
+console.log(markup);
+
+const indoor;
+console.log(indoor);
+
+const outdoor;
+console.log(outdoor);
+
+const playground;
+console.log(playground);
+
+const plugout;
+console.log(plugout);
+
+const armyleader;
+console.log(armyleader);
+
+const teamleader;
+console.log(teamleader);
+
+const packageoffer;
+console.log(packageoffer);
+
+const bedsheet;
+console.log(bedsheet);
+
+const leapyear;
+console.log(leapyear);
+
+const favicon;
+console.log(favicon);
+
+const youngerbrother;
+console.log(youngerbrother);
+
+const elderbrother;
+console.log(elderbrother);
+
+const fileeditor;
+console.log(fileeditor);
+
+const fullstack;
+console.log(fullstack);
+
+const frontend;
+console.log(frontend);
+
+const backend;
+console.log(backend);
+
+const fontsize;
+console.log(fontsize);
+
+const legpain;
+console.log(legpain);
+
+const linebreak;
+console.log(linebreak);
+
+const linenumber;
+console.log(linenumber);
+
+const outline;
+console.log(outline);
+
+const introline;
+console.log(introline);
+
+const outdooractivity;
+console.log(outdooractivity);
+
+const washingmachine;
+console.log(washingmachine);
+
+const breakdance;
+console.log(breakdance);
+
+const systemmonitor;
+console.log(systemmonitor);
+
+const systemmouse;
+console.log(systemmouse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
