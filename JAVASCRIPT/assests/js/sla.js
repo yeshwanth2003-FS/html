6c33acc9bc9e6291f2e949a404da69dfcb97944c
yeshwanth2003-FS <yeshwanthyeshwanth2003@gmail.com>
2026-06-08 12:57:20 +0530

    updated

---
+++ JAVASCRIPT/assests/js/sla.js
@@ -0,0 +1,36 @@
+let totalCourses=15;
+console.log(typeof totalCourses,"totalCourses=", totalCourses);
+
+let totalClassroom=10;
+console.log(typeof totalClassroom,"totalclassroom=",totalClassroom);
+
+let coursesDuration="4 months"
+console.log(typeof coursesDuration,"coursesDuration=",coursesDuration);
+
+let coursesFees="40k"
+console.log(typeof coursesFees,"coursesFees=",coursesFees);
+
+let totalstaffs=15;
+console.log(typeof totalstaffs,"totalstaff=",totalstaffs);
+
+let staffSalary="2c";
+console.log(typeof staffSalary,"staffSalary=",staffSalary);
+
+let placementisThere=true;
+console.log(typeof placementisThere,"placement is there=",placementisThere );
+
+let totalStudents=150;
+console.log(typeof totalStudents,"total student",totalStudents);
+
+let  totalfloor=4;
+console.log(typeof totalfloor,"totalfloor=", totalfloor);
+
+let smokingallowed="";
+console.log(typeof smokingallowed,"smokingallowed=",smokingallowed);
+
+co
+
+
+
+
+
