d4e1f4e9a6c10e078bc91548ce6b2930e74712e4
yeshwanth2003-FS <yeshwanthyeshwanth2003@gmail.com>
2026-06-29 17:38:33 +0530

    updated

---
+++ JAVASCRIPT/day14/code.js
@@ -0,0 +1,9 @@
+let detailes=[]
+
+let userName=document.getElementById("username").value
+let useremail=document.getElementById("useremail").value
+let userPassword=document.getElementById("userpassword").value
+
+let regi={userName,useremail,userPassword}
+
+console.log(regi);
